33e0d9f9c25abcee6d627b9896fa036e97d65f34
Rodrigo Mello <rodrigo@localhost.localdomain>
2017-08-06 02:07:21 -0300

    Arrastar e soltar básico funcional, mas requer ajustar state

--- componente/exemplos/04-lista-arrastar-e-soltar/src/componente/ListaArrastarSoltar/index.jsx
+++ componente/exemplos/04-lista-arrastar-e-soltar/src/componente/ListaArrastarSoltar/index.jsx
@@ -24,24 +24,28 @@ class ListaArrastarSoltar extends Component {
         let pageY = event.touches[0].pageY;
         let elementTop = event.target.getBoundingClientRect().top;
         let elementHeight = event.target.offsetHeight;
-        console.log('### elementHeight');
-        console.log(elementHeight);
-
         let lista = Array.prototype.slice.call(this.lista.getElementsByClassName('item'));
         lista.forEach((item, i) => {
             let top = item.getBoundingClientRect().top;
             if (top < elementTop && top + elementHeight - 25 > elementTop) {
                 item.classList.add('acima');
-                lista.forEach((item_2, i_2) => {
-                    if (item === item_2) {
+                lista.forEach((item_2) => {
+                    if (!item.isEqualNode(item_2)) {
                         item_2.classList.remove('acima');
                     }
                 });
             }
         });
-
         event.target.style.position = 'absolute';
         event.target.style.top = parseInt((pageY - (elementHeight / 2))) + 'px';
+    }
+    onTouchEnd(index, event) {
+        let removido = this.state.lista.splice(index, 1);
+        let ordem = this.lista.getElementsByClassName('acima')[0].dataset.ordem;
+        this.state.lista.splice(ordem, 0, removido[0]);
+        this.setState({
+            lista: this.state.lista
+        });
     }
     render() {
         return (
@@ -52,8 +56,10 @@ class ListaArrastarSoltar extends Component {
                 { this.state.lista.map((item, i) => {
                     return (
                         <div
-                            key={ i }
                             className='item'
+                            key={ i }
+                            data-ordem={ i }
+                            onTouchEnd={ this.onTouchEnd.bind(this, i) }
                             onTouchMove={ this.onTouchMove.bind(this) }
                         >
                             { item.texto }
