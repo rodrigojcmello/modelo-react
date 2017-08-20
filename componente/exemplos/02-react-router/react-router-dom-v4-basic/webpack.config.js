749237c3db7598313ac538398bbe75354825dd79
Rodrigo Mello <rodrigojcmello@gmail.com>
2017-08-20 09:54:15 -0300

    Ajuste na rota

--- componente/exemplos/02-react-router/react-router-dom-v4-basic/webpack.config.js
+++ componente/exemplos/02-react-router/react-router-dom-v4-basic/webpack.config.js
@@ -2,11 +2,25 @@
 const html = require('html-webpack-plugin');
 
 const config = {
-    entry: './index.jsx',
+    entry: './src/index.jsx',
     output: { filename: 'pacote.min.js' },
     resolve: { extensions: ['.js', '.jsx'] },
-    module: { loaders: [{ test: /\.jsx?$/, use: 'babel-loader' }]},
-    plugins: [ new html({ template: 'index.html' }) ]
+    module: { rules: [
+        {
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['stage-3', 'es2015', 'react']
+            },
+            include: __dirname + '/src'
+        }
+    ] },
+    plugins: [
+        new html({
+            template: './src/index.html',
+            inject: false
+        })
+    ]
 };
 
 module.exports = config;
